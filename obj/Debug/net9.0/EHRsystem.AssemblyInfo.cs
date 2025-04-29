bc1800e811b9abca864b26fc34faba74045fbe3d
FatmaNageh <146003045+FatmaNageh@users.noreply.github.com>
2025-04-29 17:57:18 +0300

    Resolved merge conflicts in Error.cshtml and related files

--- obj/Debug/net9.0/EHRsystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/EHRsystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EHRsystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7175b4b47b1b548dabe53fd898fe9ef4afbe4de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d563e4cb18dbf246d590f7a0746538ba9cf970")]
 [assembly: System.Reflection.AssemblyProductAttribute("EHRsystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EHRsystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
